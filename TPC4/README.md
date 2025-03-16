579e65c8fdb85f5016a3dba58f7aef75b2578880
Ze-Limao <jeswitch.24@gmail.com>
2025-03-16 19:51:02 +0000

    b

---
+++ TPC4/README.md
@@ -0,0 +1,6 @@
+# Lista ON/OFF
+### Exemplo
+```
+Lista 1, agora, 2, agora, 3, fim, 4, fim, 5, 6, agora, 10, fim .
+```
+A soma é 19.
--- TPC4/listasAgoraFim.py
+++ TPC4/listasAgoraFim.py
@@ -1,46 +1,60 @@
 from lark import Lark, Transformer, v_args
+from collections import Counter
 
 class intervalTransformerAcceptence(Transformer):
     '''
-    Atributes:
-        sentido (int): Represents the current direction of the signal (+1 or -1).
-        anterior (float): Stores the endpoint of the previous interval.
+    Attributes:
+        agora: Flag indicating the start of an interval.
+        fim: Flag indicating the end of an interval.
         erro (bool): Flag indicating whether a constraint violation occurred.
     '''
     def __init__(self):
         self.started = 0
         self.podeAcabar = []
         self.erro = False
+        self.sum = 0
+        self.elements = []
 
     @v_args(inline=True)
     def valor(self, valor):
         if valor == "agora":
-            self.started +=1
+            self.started += 1
             self.podeAcabar.append(False)
         elif valor == "fim":
             if self.started == 0:
-                print(f"Error: Constraint CC1 violated. agora should appead before the fim).")
+                print(f"Error: Constraint CC1 violated. 'agora' should appear before 'fim'.")
                 self.erro = True
                 return None  # Skip invalid interval
             if not self.podeAcabar[-1]:
-                print(f"Error: Constraint CC1 violated. should appear a number between agora and fim).")
+                print(f"Error: Constraint CC1 violated. A number should appear between 'agora' and 'fim'.")
                 self.erro = True
                 return None  # Skip invalid interval
-            self.started -=1
+            self.started -= 1
             self.podeAcabar.pop()
         else:
             if self.started != 0:
                 self.podeAcabar[-1] = True
+                self.sum += int(valor)
+                self.elements.append(int(valor))
 
-        return {"ok"}
+        return valor
 
     @v_args(inline=True)
     def valores(self, *valores):
         return [valor for valor in valores if valor is not None]
 
     @v_args(inline=True)
-    def sentence(self, a,valores):
-        return {"valores": valores}
+    def sentence(self, L, valores):
+        element_count = Counter(self.elements)
+        max_freq = max(element_count.values())
+        most_frequent = [el for el, freq in element_count.items() if freq == max_freq]
+
+        return {
+            "valores": valores,
+            "soma": self.sum,
+            "mais_frequentes": most_frequent,
+            "n_elemtos": len(self.elements)
+        }
 
     @v_args(inline=True)
     def start(self, sentence):
@@ -63,7 +77,10 @@ def main():
     #tree = parser.parse("LISTA 1 .")
     #print(tree.pretty())
 
-    tree = parser.parse("Lista 1, 2, agora, 3, 4, fim, 7, 8.")
+    #tree = parser.parse("Lista 1, 2, agora, 3, 4, fim, 7, 8.")
+    #print(tree.pretty())
+
+    tree = parser.parse("Lista 1, agora, 2, agora, 3, fim, 4, fim, 5, 6, agora, 10, fim .")
     print(tree.pretty())
 
     try:
